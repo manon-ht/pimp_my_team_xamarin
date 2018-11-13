5c258e90417c73c4b3dd3317e21cef509f92ae55
manon herbert <manonh@picto.fr>
2018-11-13 10:50:26 +0100

    Tests

---
+++ PimpMyTeam/PimpMyTeam/Teams/TeamPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PimpMyTeam.Teams
+{
+    public class TeamPage : ContentPage
+    {
+        public TeamPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
